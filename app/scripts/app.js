30012bd2c690e8551973a56509e2a92acb8072ec
Kun <likunarmstrong@gmail.com>
2015-08-27 15:43:09 -0600

    Fix the issue which prevent Safari finding elements

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,15 @@
 /*jshint -W079 */
 /*jshint unused:false*/
+/*jshint -W079 */
+var processor = new Worker('/profile/scripts/processor.js');
+
+var workoutFetching = function (id) {
+    processor.postMessage({
+        token: jwt.token,
+        type: 'workout',
+        id: id
+    });
+};
 
 (function(document) {
     'use strict';
@@ -10,95 +20,89 @@
     var app = document.querySelector('#app');
 
     app.displayInstalledToast = function() {
-    document.querySelector('#caching-complete').show();
+        document.querySelector('#caching-complete').show();
     };
 
+    var mapSummary;
+    var mapRun;
+    var logBook;
+    var workoutElement;
+    var header;
     // Listen for template bound event to know when bindings
     // have resolved and content has been stamped to the page
     app.addEventListener('dom-change', function() {
-    console.log('Our app is ready to rock!');
+        console.log('Our app is ready to rock!');
+        mapSummary = document.querySelector('map-summary');
+        mapRun = document.querySelector('map-run');
+        logBook = document.querySelector('log-book');
+        workoutElement = document.querySelector('workout-element');
+        processor.onmessage = function (event) {
+            if ('mapRunData' in event.data) {
+                mapRun.setData(event.data.mapRunData);
+            }
+            if ('chartData' in event.data) {
+                workoutElement.setChartData(event.data.chartData);
+            }
+            if ('logs' in event.data) {
+                logBook.populateLogs(event.data.logs);
+            }
+        };
+
+        header = document.querySelector('header-element');
+        // We use Page.js for routing. This is a Micro
+        // client-side router inspired by the Express router
+        // More info: https://visionmedia.github.io/page.js/
+        page.base('/profile');
+
+        page('/', function () {
+            logsFetching();
+            header.toggleActive('home');
+            app.route = 'home';
+        });
+
+        page('/users', function () {
+            app.route = 'users';
+        });
+
+        page('/users/:name', function (data) {
+            app.route = 'user-info';
+            app.params = data.params;
+        });
+
+        page('/settings', function () {
+            app.route = 'settings';
+            header.toggleActive('settings');
+        });
+
+        page('/contact', function () {
+            app.route = 'contact';
+        });
+
+        // add #! before urls
+        page({
+            hashbang: false,
+        });
     });
 
     app.logout = function() {
         jwt.logout();
     };
 
-    // See https://github.com/Polymer/polymer/issues/1381
     window.addEventListener('WebComponentsReady', function() {
-    // imports are loaded and elements have been registered
-    });
-
-    // Main area's paper-scroll-header-panel custom condensing transformation of
-    // the appName in the middle-container and the bottom title in the bottom-container.
-    // The appName is moved to top and shrunk on condensing. The bottom sub title
-    // is shrunk to nothing on condensing.
-    addEventListener('paper-header-transform', function(e) {
-    var appName = document.querySelector('.app-name');
-    var middleContainer = document.querySelector('.middle-container');
-    var bottomContainer = document.querySelector('.bottom-container');
-    var detail = e.detail;
-    var heightDiff = detail.height - detail.condensedHeight;
-    var yRatio = Math.min(1, detail.y / heightDiff);
-    var maxMiddleScale = 0.50;  // appName max size when condensed. The smaller the number the smaller the condensed size.
-    var scaleMiddle = Math.max(maxMiddleScale, (heightDiff - detail.y) / (heightDiff / (1-maxMiddleScale))  + maxMiddleScale);
-    var scaleBottom = 1 - yRatio;
-
-    // Move/translate middleContainer
-    Polymer.Base.transform('translate3d(0,' + yRatio * 100 + '%,0)', middleContainer);
-
-    // Scale bottomContainer and bottom sub title to nothing and back
-    Polymer.Base.transform('scale(' + scaleBottom + ') translateZ(0)', bottomContainer);
-
-    // Scale middleContainer appName
-    Polymer.Base.transform('scale(' + scaleMiddle + ') translateZ(0)', appName);
     });
 
     // Close drawer after menu item is selected if drawerPanel is narrow
     app.onMenuSelect = function() {
-    var drawerPanel = document.querySelector('#paperDrawerPanel');
-    if (drawerPanel.narrow) {
-        drawerPanel.closeDrawer();
-    }
+        var drawerPanel = document.querySelector('#paperDrawerPanel');
+        if (drawerPanel.narrow) {
+            drawerPanel.closeDrawer();
+        }
+    };
+    var logsFetching = function () {
+        processor.postMessage({
+            token: jwt.token,
+            type: 'all'
+        });
     };
 })(document);
 
-var mapSummary;
-var mapRun;
-var logBook;
-var workoutElement;
-
-window.addEventListener('WebComponentsReady', function() {
-    mapSummary = document.querySelector('map-summary');
-    mapRun = document.querySelector('map-run');
-    logBook = document.querySelector('log-book');
-    workoutElement = document.querySelector('workout-element');
-});
-
-var processor = new Worker('/profile/scripts/processor.js');
-
-processor.onmessage = function (event) {
-    if ('mapRunData' in event.data) {
-        mapRun.setData(event.data.mapRunData);
-    }
-    if ('chartData' in event.data) {
-        workoutElement.setChartData(event.data.chartData);
-    }
-    if ('logs' in event.data) {
-        logBook.populateLogs(event.data.logs);
-    }
-};
-
-var logsFetching = function () {
-    processor.postMessage({
-        token: jwt.token,
-        type: 'all'
-    });
-};
-
-var workoutFetching = function (id) {
-    processor.postMessage({
-        token: jwt.token,
-        type: 'workout',
-        id: id
-    });
-};
