2605361b283ff2746479cac2a7bb63454f2ff17b
Kun <likunarmstrong@gmail.com>
2016-06-08 14:28:07 -0600

    Plan interface visual improvement

--- app/scripts/plan.js
+++ app/scripts/plan.js
@@ -4,6 +4,17 @@
 (function(document) {
     'use strict';
 
+    // Grab a reference to our auto-binding template
+    // and give it some initial binding values
+    // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
+    let app = document.querySelector('#app');
+
+    // Listen for template bound event to know when bindings
+    // have resolved and content has been stamped to the page
+    app.addEventListener('dom-change', function() {
+        console.log('Our app is ready to rock!');
+    });
+
     page.base('/powercenter/plan');
 
     page('/', () => {
@@ -18,18 +29,6 @@
     page('/:planID', (data) => {
         app.route = 'new-plan';
         app.params = data.params;
-    });
-
-
-    // Grab a reference to our auto-binding template
-    // and give it some initial binding values
-    // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
-    let app = document.querySelector('#app');
-
-    // Listen for template bound event to know when bindings
-    // have resolved and content has been stamped to the page
-    app.addEventListener('dom-change', function() {
-        console.log('Our app is ready to rock!');
     });
 
     // See https://github.com/Polymer/polymer/issues/1381
