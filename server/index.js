0cfb858dde1d08b13e91642b8fab62a9e88e45b5
Tony Trinh <tony19@gmail.com>
2018-02-28 14:55:38 -0600

    Update server script to take root path arg

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const {startServer: startApiServer} = require('./api-server');
 const {startServer: startAppServer} = require('./app-server');
 
+const {2: rootPath} = process.argv;
+if (rootPath) {
+  process.chdir(rootPath);
+}
+
 const apiServerPort = 8888;
 startAppServer(apiServerPort);
 startApiServer(apiServerPort);
