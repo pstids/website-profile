3ac2cd3b4b03222c78ba483fc4b4a99fbf6cdb70
Tony Trinh <tony19@gmail.com>
2018-03-02 19:23:24 -0600

    Fix runtime error in processor.js

--- app/scripts/processor.js
+++ app/scripts/processor.js
@@ -8,6 +8,7 @@
 
 importScripts('dexie.js');
 importScripts('superagent.js');
+importScripts('toolbox.js');
 
 var data = {};
 var guestToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imd1ZXN0QHN0cnlkLmNvbSIsImV4cCI6NDYwNDk2MjEwMTk1NiwiZmlyc3RuYW1lIjoiU3RyeWQiLCJpZCI6Ii0xIiwiaW1hZ2UiOiIiLCJsYXN0bmFtZSI6IlJ1bm5lciIsInVzZXJuYW1lIjoiZ3Vlc3QifQ.jlm3nYOYP_L9r8vpOB0SOGnj5t9i8FWwpn5UxOfar1M';
